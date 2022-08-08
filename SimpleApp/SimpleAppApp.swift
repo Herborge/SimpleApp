8ea38ed2ca6d504368e2693325e6eac4b248c2af
herborge <herborge@gmail.com>
2022-08-08 12:32:24 -0300

    Initial Commit

---
+++ SimpleApp/SimpleAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SimpleAppApp.swift
+//  SimpleApp
+//
+//  Created by Hercules Borges Rodrigues on 08/08/22.
+//
+
+import SwiftUI
+
+@main
+struct SimpleAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
